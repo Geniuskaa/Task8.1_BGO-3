0d2c133f46fef966d29be550dde6e98fb1412303
Geniuskaa <eminizinar@gmail.com>
2021-02-05 09:58:33 +0300

    Initial commit

---
+++ cmd/tcpserver/tcpserver.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"bufio"
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	if err := execute(); err != nil {
+		os.Exit(1)
+	}
+}
+
+func execute() (err error) {
+	listener, err := net.Listen("tcp", "0.0.0.0:9999")
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	defer func() {
+		if cerr := listener.Close(); cerr != nil {
+			log.Println(cerr)
+			if err == nil {
+				err = cerr
+			}
+		}
+	}()
+
+	for  {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		handle(conn)
+	}
+}
+
+func handle(conn net.Conn) {
+	defer func() {
+		if cerr := conn.Close(); cerr != nil {
+			log.Println(cerr)
+		}
+	}()
+
+	reader := bufio.NewReader(conn)
+	const delim = '\n'
+
+	line, err := reader.ReadString(delim)
+	if err != nil {
+		if err != io.EOF {
+			log.Println(err)
+		}
+		log.Printf("received: %s\n", line)
+		return
+	}
+	log.Printf("received: %s\n", line)
+
+	writer := bufio.NewWriter(conn)
+	_, err = writer.WriteString("Hello from go!")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Geniuskaa/Task8.1_BGO-3
+
+go 1.15
